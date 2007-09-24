90cd87dd75d49b4714e165b31673de39512e000c
mike.rettig <mike.rettig@b3a869e6-0839-0410-a182-4b2ee099aa3d>
2007-09-24 20:19:59 +0000

    Removed file/folder

--- tags/0.2.1/src/Retlang/IObjectPublisher.cs
+++
@@ -1,8 +0,0 @@
-namespace Retlang
-{
-    public interface IObjectPublisher
-    {
-        void Publish(object topic, object msg, object replyToTopic);
-        void Publish(object topic, object msg);
-    }
-}
