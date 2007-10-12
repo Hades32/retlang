5e4ab50816128646a5d4507ae04b68e8d843362c
mike.rettig <mike.rettig@b3a869e6-0839-0410-a182-4b2ee099aa3d>
2007-10-12 01:33:32 +0000

    add docs.

--- trunk/src/Retlang/ITransferEnvelope.cs
+++ trunk/src/Retlang/ITransferEnvelope.cs
@@ -2,9 +2,19 @@ using System;
 
 namespace Retlang
 {
+    /// <summary>
+    /// Packages a message for transfer between contexts.
+    /// </summary>
     public interface ITransferEnvelope
     {
+        /// <summary>
+        /// The type of the message contained in the envelope.
+        /// </summary>
         Type MessageType { get; }
+
+        /// <summary>
+        /// The topic and reply topic of the message.
+        /// </summary>
         IMessageHeader Header { get; }
 
         /// <summary>
--- trunk/src/Retlang/ObjectTransferEnvelope.cs
+++ trunk/src/Retlang/ObjectTransferEnvelope.cs
@@ -2,11 +2,17 @@ using System;
 
 namespace Retlang
 {
+    /// <summary>
+    /// Transfer objects between contexts without serialization.
+    /// </summary>
     public class ObjectTransferEnvelope : ITransferEnvelope
     {
         private readonly IMessageHeader _header;
         private readonly object _obj;
-
+        /// <summary>
+        /// </summary>
+        /// <param name="obj">data to publish</param>
+        /// <param name="header">header</param>
         public ObjectTransferEnvelope(object obj, IMessageHeader header)
         {
             if (obj == null)
