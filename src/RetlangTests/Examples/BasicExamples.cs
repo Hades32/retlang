8c626b7c0dde010d3b586b7195e13f5cf502d867
mike.rettig <mike.rettig@b3a869e6-0839-0410-a182-4b2ee099aa3d>
2008-11-21 01:11:39 +0000

    add examples.

---
+++ src/RetlangTests/Examples/BasicExamples.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using Retlang.Channels;
+using Retlang.Fibers;
+
+namespace RetlangTests.Examples
+{
+    [TestFixture]
+    public class BasicExamples
+    {
+        [Test]
+        public void batching()
+        {
+            using (IFiber fiber = new ThreadFiber())
+            {
+                fiber.Start();
+                var counter = new Channel<int>();
+                var reset = new ManualResetEvent(false);
+                int total = 0;
+                Action<IList<int>> cb = delegate(IList<int> batch)
+                                            {
+                                                total += batch.Count;
+                                                if (total == 10)
+                                                {
+                                                    reset.Set();
+                                                }
+                                            };
+
+                counter.SubscribeToBatch(fiber, cb, 1);
+
+                for (int i = 0; i < 10; i++)
+                {
+                    counter.Publish(i);
+                }
+
+                Assert.IsTrue(reset.WaitOne(10000));
+            }
+        }
+
+        [Test]
+        public void batchingWithKey()
+        {
+            using (IFiber fiber = new ThreadFiber())
+            {
+                fiber.Start();
+                var counter = new Channel<int>();
+                var reset = new ManualResetEvent(false);
+                Action<IDictionary<String, int>> cb = delegate(IDictionary<String, int> batch)
+                {
+                    if (batch.ContainsKey("9"))
+                    {
+                        reset.Set();
+                    }
+                };
+
+                Converter<int, String> keyResolver = x => x.ToString();
+                counter.SubscribeToKeyedBatch(fiber, keyResolver, cb, 0);
+
+                for (int i = 0; i < 10; i++)
+                {
+                    counter.Publish(i);
+                }
+
+                Assert.IsTrue(reset.WaitOne(10000));
+            }
+        }
+
+
+        [Test]
+        public void pubSubWithPool()
+        {
+            using (IFiber fiber = new PoolFiber())
+            {
+                fiber.Start();
+                var channel = new Channel<string>();
+
+                var reset = new AutoResetEvent(false);
+                channel.Subscribe(fiber, delegate { reset.Set(); });
+                channel.Publish("hello");
+
+                Assert.IsTrue(reset.WaitOne(5000));
+            }
+        }
+
+        [Test]
+        public void pubSubWithThread()
+        {
+            using (IFiber fiber = new ThreadFiber())
+            {
+                fiber.Start();
+                var channel = new Channel<string>();
+
+                var reset = new AutoResetEvent(false);
+                channel.Subscribe(fiber, delegate { reset.Set(); });
+                channel.Publish("hello");
+
+                Assert.IsTrue(reset.WaitOne(5000));
+            }
+        }
+    }
+}
