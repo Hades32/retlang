8d8771131189cff5b0b8e958837844c59bc50a7f
graham.m.nash <graham.m.nash@b3a869e6-0839-0410-a182-4b2ee099aa3d>
2009-02-14 01:13:38 +0000

    0.4.1.6

--- src/Retlang/Core/IBatchAndSingleExecutor.cs
+++ src/Retlang/Core/IBatchAndSingleExecutor.cs
@@ -2,8 +2,15 @@
 
 namespace Retlang.Core
 {
+    ///<summary>
+    /// Executes pending action(s).
+    ///</summary>
     public interface IBatchAndSingleExecutor : IBatchExecutor
     {
+        ///<summary>
+        /// Execute a single pending action.
+        ///</summary>
+        ///<param name="action"></param>
         void Execute(Action action);
     }
 }
