b0c02f75612dde358be30c22d2b33e7cceea7023
mike.rettig <mike.rettig@b3a869e6-0839-0410-a182-4b2ee099aa3d>
2009-02-16 01:38:15 +0000

    Add WPF example.

---
+++ src/WpfExample/Window1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Retlang.Fibers;
+
+namespace WpfExample
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private readonly WindowChannels channels = new WindowChannels();
+        private readonly IFiber fiber;
+
+        public Window1()
+        {
+            InitializeComponent();
+            fiber = new DispatcherFiber(this.Dispatcher);
+            fiber.Start();
+            channels.TimeUpdate.SubscribeToLast(fiber, OnTimeUpdate, 0);
+            UpdateController update = new UpdateController(channels);
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            channels.StartChannel.Publish(e);
+        }
+
+        private void OnTimeUpdate(DateTime time)
+        {
+            this.cpuTextBox.Text = time.ToString();
+        }
+    }
+}
---
+++ src/WpfExample/WindowChannels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Retlang.Channels;
+
+namespace WpfExample
+{
+    public class WindowChannels
+    {
+        public readonly IChannel<DateTime> TimeUpdate = new Channel<DateTime>();
+        public readonly IChannel<RoutedEventArgs> StartChannel = new Channel<RoutedEventArgs>();
+    }
+}
