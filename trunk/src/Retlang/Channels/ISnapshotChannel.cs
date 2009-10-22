23974bb24fbaf5bb4a76f1c7efdc9719a757bfd1
graham.m.nash <graham.m.nash@b3a869e6-0839-0410-a182-4b2ee099aa3d>
2009-10-22 20:47:36 +0000

    Adding SnapshotChannel.  Provides ability to get snapshot of entire state synchronously, then get subsequent changes in state asynchronously.

---
+++ trunk/src/Retlang/Channels/ISnapshotChannel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Retlang.Core;
+
+namespace Retlang.Channels
+{
+    public interface ISnapshotChannel<T>
+    {
+        void PrimedSubscribe(IDisposingExecutor fiber, Action<T> handler);
+        void Publish(T update);
+        void ReplyToPrimingRequest(IDisposingExecutor fiber, Func<T> getter);
+    }
+}
---
+++ trunk/src/Retlang/Channels/SnapshotChannel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Retlang.Core;
+
+namespace Retlang.Channels
+{
+    public class SnapshotChannel<T> : ISnapshotChannel<T>
+    {
+        private readonly int _timeoutInMs;
+        private readonly IChannel<T> _updatesChannel = new Channel<T>();
+        private readonly RequestReplyChannel<object, T> _requestChannel = new RequestReplyChannel<object, T>();
+
+        public SnapshotChannel(int timeoutInMs)
+        {
+            _timeoutInMs = timeoutInMs;
+        }
+
+        public void PrimedSubscribe(IDisposingExecutor fiber, Action<T> handler)
+        {
+            using (var reply = _requestChannel.SendRequest(new object()))
+            {
+                if (reply == null)
+                {
+                    throw new ArgumentException(typeof (T).Name + " synchronous request has no reply subscriber.");
+                }
+
+                T result;
+                if (!reply.Receive(_timeoutInMs, out result))
+                {
+                    throw new ArgumentException(typeof (T).Name + " synchronous request timed out in " + _timeoutInMs);
+                }
+
+                handler(result);
+
+                _updatesChannel.Subscribe(fiber, handler);
+            }
+        }
+
+        public void Publish(T update)
+        {
+            _updatesChannel.Publish(update);
+        }
+
+        public void ReplyToPrimingRequest(IDisposingExecutor fiber, Func<T> getter)
+        {
+            _requestChannel.Subscribe(fiber, request => request.SendReply(getter()));
+        }
+    }
+}
