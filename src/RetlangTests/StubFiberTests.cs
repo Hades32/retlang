b8244b4d95622397f05defb61ec9e91ba9806cc5
graham.m.nash <graham.m.nash@b3a869e6-0839-0410-a182-4b2ee099aa3d>
2010-01-07 20:49:56 +0000

    Deleted broken ScheduledEvent that only was used in StubFiber Fixed StubFiber so that ScheduleOnInterval recur and Schedule actions do not.

--- src/RetlangTests/StubFiberTests.cs
+++ src/RetlangTests/StubFiberTests.cs
@@ -33,5 +33,31 @@ namespace RetlangTests
             fiber.ExecuteAllPendingUntilEmpty();
             Assert.AreEqual(new[] { fired1, fired2, fired3 }, actionMarkers.ToArray());
         }
+
+        [Test]
+        public void ScheduledTasksShouldBeExecutedOnceScheduleIntervalShouldBeExecutedEveryTimeExecuteScheduleAllIsCalled()
+        {
+            var fiber = new StubFiber();
+
+            var scheduleFired = 0;
+            var scheduleOnIntervalFired = 0;
+
+            fiber.Schedule(() => scheduleFired++, 100);
+            var intervalSub = fiber.ScheduleOnInterval(() => scheduleOnIntervalFired++, 100, 100);
+
+            fiber.ExecuteAllScheduled();
+            Assert.AreEqual(1, scheduleFired);
+            Assert.AreEqual(1, scheduleOnIntervalFired);
+
+            fiber.ExecuteAllScheduled();
+            Assert.AreEqual(1, scheduleFired);
+            Assert.AreEqual(2, scheduleOnIntervalFired);
+
+            intervalSub.Cancel();
+
+            fiber.ExecuteAllScheduled();
+            Assert.AreEqual(1, scheduleFired);
+            Assert.AreEqual(2, scheduleOnIntervalFired);
+        }
     }
 }
